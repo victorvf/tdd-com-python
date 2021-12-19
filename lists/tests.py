513e70743d88154f4f74eed126b4594650d32dfa
˜victorvf˜ <victorvitoria11@hotmail.com>
2021-12-19 09:48:00 -0300

    refactor: view da pagina inicial agora usando template

--- lists/tests.py
+++ lists/tests.py
@@ -6,6 +6,9 @@ from lists.views import home_page
 
 
 class SmokeTest(TestCase):
+    """
+    Primeiros testes:
+
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve("/")
         self.assertEqual(found.func, home_page)
@@ -17,4 +20,9 @@ class SmokeTest(TestCase):
 
         self.assertTrue(html.startswith("<html>"))
         self.assertIn("<title>To-Do lists</title>", html)
-        self.assertTrue(html.endswith("</html>"))
+        self.assertTrue(html.strip().endswith("</html>"))
+    """
+
+    def test_uses_home_template(self):
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "lists/home.html")
